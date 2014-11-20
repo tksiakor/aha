0821345324ec79ed51d6b4f12f291e73c44c3d3d
SenanuFC <senanu.fiam-coblavie@ashesi.edu.gh>
2014-11-20 20:26:34 +0000

    Corrected syntax error v

--- server/aha.js
+++ server/aha.js
@@ -6,7 +6,7 @@ var gen = require('ical-generator'),
 	http = require('http');
 var gm = require('googlemaps');
 var util = require('util');	
-v//ar geolib = require('geolib');
+//ar geolib = require('geolib');
 	
 mongoose.connect('mongodb://127.0.0.1/ahaSchema'); 
 
